194bae766b4e0a8b2620e8d0eb769599c41dc4da
ilgulee <iglee2000ca@gmail.com>
2017-09-29 04:10:46 -0400

    Setup Lab3

---
+++ COMP305-F2017-Lab3/Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+
+    private Rigidbody2D rBody;
+	// Use this for initialization
+	void Start ()
+	{
+	    rBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+	    float horizMove = Input.GetAxis("Horizontal");
+	    float verMove = Input.GetAxis("Vertical");
+        Vector3 movement=new Vector3(horizMove,verMove,0);
+	    rBody.velocity = movement * speed;
+
+	}
+}
